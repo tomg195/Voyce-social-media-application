82ffa63bef7a0553f3619c4b132ce4eb95a027be
tomg195 <gardner195@msn.com>
2024-01-22 11:58:03 +0000

    loginpage

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={require("./logo.png")} style={styles.logo} />
+      <TextInput
+        style={styles.loginInput}
+        placeholder="Username"
+        placeholderTextColor="#888"
+      />
+      <TextInput
+        style={styles.loginInput}
+        placeholder="Password"
+        placeholderTextColor="#888"
+        secureTextEntry={true}
+      />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>SignUp</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +40,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    paddingTop: 50,
+  },
+  logo: {
+    marginBottom: -50,
+    width: 310,
+    height: 400,
+    resizeMode: "contain",
+  },
+  loginInput: {
+    // paddingTop: 50,
+    height: 40,
+    borderColor: "silver",
+    borderWidth: 1,
+    width: 250,
+    marginVertical: 7,
+    paddingLeft: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "70%",
+    // marginTop: 20,
+  },
+  button: {
+    backgroundColor: "midnightblue",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
   },
 });
